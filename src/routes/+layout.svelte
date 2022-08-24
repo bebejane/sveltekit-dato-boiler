3d898fcf205600ef71b15666136979404c498988
Bebe Jane <bjornthief@gmail.com>
2022-08-24 20:42:57 +0200

    pagey

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,47 @@
 <script>
-	export let data;
-	const menu = data.allMenus
-	
+export let data;
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="/global.css">
-</svelte:head>
+    <link rel="stylesheet" href="/global.css">
+    </svelte:head>
 
-<main>
-	<ul>
-		{#each menu as item }
-			<li><a href="/">{item.title}</a></li>
-		{/each}
-	</ul>
-	<slot/>
-</main>
+    <nav>
+        <ul>
+            <li>Menu</li>
+        </ul>
+    </nav>
+    <main>
+        <slot/>
+            </main>
 
 <style lang="scss">
-	
-	:root {
-		--navbar-height: 65px;
-	}
-	main {
-		padding: var(--padding);
-		padding-top: var(--navbar-height);
+:root {
+  --navbar-height: 65px;
+}
 
-	}
-	ul{
-		position: fixed;
-		top:0;
-		left:0;
-		width: 100%;
-		background-color: #fff;
-		padding-left: var(--padding);
-		max-height: var(--navbar-height);
-		min-height: var(--navbar-height);
-		margin: 0;
-		list-style: none;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		li{
-			margin-right: var(--gutter);
-		}
-	}
+main {
+  padding: var(--padding);
+  padding-top: var(--navbar-height);
+}
 
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  color: #fff;
+  background-color: rgb(65, 33, 80);
+  min-height: calc(var(--navbar-height));
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: calc(var(--navbar-height));
+  margin: 0;
+  padding-left: 20px;
+  list-style: none;
+}
 </style>
--- src/routes/post/[slug]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "$lib/client";
-import { Post } from "$lib/graphql/posts.gql";
-
-export async function load({ params }) {
-  
-  let { data: { post } } = await client.query(Post, {variables:{slug:params.slug}}).result();
-  return { post }
-}
