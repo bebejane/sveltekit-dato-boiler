e6af4ce225df3e14c6f8865084bfbf85bce82fd6
Bebe Jane <bjornthief@gmail.com>
2022-08-25 12:47:22 +0200

    blogggg

---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from '../../../../.svelte-kit/types/src/routes/post/[slug]/$types';
+	export let data: PageData;
+	const { post } = data;
+</script>
+
+<div id="posts">
+	{post.title}
+</div>
+
+<style lang="scss">
+</style>
